119e3a7478e8d5d4da5258177cea2106a2699b29
christine gomez <christine.gomez1226@gmail.com>
2018-08-28 16:43:35 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,10 +2,7 @@
 
 def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  loop do
-  puts phrase
-  break if loop == 7
-  end
+
 end
 
 def times_iterator(number_of_times)
